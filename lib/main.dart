fa053ae2d1b7561e629f406e9198e35a95586a0b
fuadtanjung <fuad6755@gmail.com>
2021-03-26 10:57:21 +0700

    text and widget

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold( 
-      appBar: AppBar(title: Text("Aplikasi Hello World"),),
-      body: Text("Hello World"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Aplikasi Hello World"),
+        ),
+        body: Center(
+            child: Container(
+                color: Colors.lightBlue,
+                width: 150,
+                height: 50,
+                child: Text(
+                  "Saya Sedang Melatih Kemampuan Flutter saya",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 20),
+                ))),
       ),
     );
   }
